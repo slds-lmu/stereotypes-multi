f90978aa744d7c3d3e714ba5169117f4cf472649
itolgaozturk <t.oztuerk@tvy70fn61t.fritz.box>
2023-01-10 16:56:21 +0100

    removed the old fine-tuned mT5 model link

--- code/models/download_models.sh
+++ code/models/download_models.sh
@@ -1,7 +1,6 @@
 mkdir pretrained_models
 wget -P pretrained_models https://mega.nz/file/NOMWTQLb#WAcKW7JwnscYNizfvdArXKqpvCnmgEQKKrxH3z_Ez9E
 wget -P pretrained_models https://mega.nz/file/4DslXbCD#9A9KGYeOAZZSRrDg2NNKdPN_yw5WA5YWRUdKSPIReu8
-wget -P pretrained_models https://mega.nz/file/UC91xboK#tsj02CT9XHNVWk1T7kCfSce0OUwQM6krOad1NckUbhM
 wget -p pretrained_models https://mega.nz/file/qrZmXJ4Q#h_fNMZYFUeQDT9e_ANZYkhVNAxkrXZGiSutFzQ4RjdM
 wget -p pretrained_models https://mega.nz/file/3mY0mZrB#QofWiMU9sRZ8dFfNAWER0ZfhF6N4IIwG1wC3IdyQ5Ww
 wget -p pretrained_models https://mega.nz/file/H6IXSY4L#SoaGO27nTcaHMgnK4f3rvAIHFgzMrfNd3OVPelD0yMs
